4a10796a84be384c4b21b86a0a7612ef13319f15
PROJECT <32355369+amanyabda@users.noreply.github.com>
2018-12-01 13:44:41 +0200

    Add files via upload

---
+++ app/src/main/java/com/emuniapp/emuni/Activity/NotificationActivity.java
@@ -0,0 +1,54 @@
+package com.emuniapp.emuni.Activity;
+
+import android.app.NotificationManager;
+import android.app.RemoteInput;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.emuniapp.emuni.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    // Key for the string that's delivered in the action's intent.
+    private static final String KEY_TEXT_REPLY = "key_text_reply";
+
+    // mRequestCode allows you to update the notification.
+    int mRequestCode = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        TextView textView = (TextView) findViewById(R.id.replyMessage);
+        textView.setText(getMessageText(getIntent()));
+
+        String returnMessage = "Thank you";
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentText(returnMessage);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //update notification
+        mNotificationManager.notify(mRequestCode, mBuilder.build());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT_WATCH)
+    private CharSequence getMessageText(Intent intent) {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null) {
+            return remoteInput.getCharSequence(KEY_TEXT_REPLY);
+        }
+        return null;
+    }
+}
